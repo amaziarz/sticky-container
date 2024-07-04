26af3edb669bc21fba658e57857882f2a138881f
Adam Maziarz <amaziarz.dev@gmail.com>
2024-07-04 15:10:25 +0200

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    // height set to 300vh to make the page scrollable for testing purposes
+    <main className="container mx-auto px-4 py-16 h-[300vh]">
+      <h1 className="mb-8 text-3xl font-bold">GitHub Search</h1>
+      <div>reference element</div>
+    </main>
+  );
+}
